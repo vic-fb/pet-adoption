dcff950bf836f687eeb41b5ca0b6ff72539ebe22
Vicky Fernandez Busch <mvfbusch@gmail.com>
2023-01-08 20:14:10 +0100

    feat(app): Add search component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { createRoot } from "react-dom";
-import Pet from "./Pet";
+import { createRoot } from "react-dom/client";
+import SearchParams from "./SearchParams";
 
 const App = () => (
   <div>
     <h1>Adopt Me!</h1>
-    <Pet name="Luna" animal="Dog" breed="Havanese" />
-    <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-    <Pet name="Doink" animal="Cat" breed="Mixed" />
+    <SearchParams />
   </div>
 );
 
