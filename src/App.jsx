3f75b5a0cdecc8d5fec5163f621414bb3d8b6de5
Vicky Fernandez Busch <mvfbusch@gmail.com>
2023-01-08 19:41:26 +0100

    feat(app): Transform pure React to JSX

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { createRoot } from "react-dom";
+import Pet from "./Pet";
+
+const App = () => (
+  <div>
+    <h1>Adopt Me!</h1>
+    <Pet name="Luna" animal="Dog" breed="Havanese" />
+    <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+    <Pet name="Doink" animal="Cat" breed="Mixed" />
+  </div>
+);
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
