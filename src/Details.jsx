a7d385c0191d20e09ef41f8c7ffae1b9455bab74
Vicky Fernandez Busch <mvfbusch@gmail.com>
2023-01-11 11:42:05 +0100

    feat(app): Create Carousel class component

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
+import Carousel from "./Carousel";
 import fetchPet from "./fetchPet";
 
 const Details = () => {
@@ -16,6 +17,7 @@ const Details = () => {
   const pet = results.data.pets[0];
   return (
     <div className="details">
+      <Carousel images={pet.images} />
       <h1>{pet.name}</h1>
       <h2>
         {`${pet.animal} — ${pet.breed} — ${pet.city}, ${pet.state}`}
