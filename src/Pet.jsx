86e440bfed721a3123fed9863916c247f0985408
Vicky Fernandez Busch <mvfbusch@gmail.com>
2023-01-19 10:41:07 +0100

    test(pet): Add test cases for component

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,14 +1,19 @@
 import { Link } from "react-router-dom";
 const Pet = ({ name, animal, breed, images, location, id }) => {
   let hero = "https://pets-images.dev-apis.com/pets/none.jpg";
-  if (images.length) {
+  if (images && images.length) {
     hero = images[0];
   }
 
   return (
     <Link to={`/details/${id}`} className="relative block">
       <div className="h-full">
-        <img src={hero} alt={name} className="aspect-square h-full w-full" />
+        <img
+          data-testid="thumbnail"
+          src={hero}
+          alt={name}
+          className="aspect-square h-full w-full"
+        />
       </div>
       <div className="absolute bottom-0 left-0 w-full bg-gradient-to-tr from-white to-transparent p-2">
         <h1>{name}</h1>
